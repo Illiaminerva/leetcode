5955bfacc97886927facc6f3a415533275645741
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-11-13 15:22:11 -0800

    [17] [Time Beats: 100.00%] [Memory Beats: 72.94%] - LeetPush

--- 17-Letter-Combinations-of-a-Phone-Number.py
+++ 17-Letter-Combinations-of-a-Phone-Number.py
@@ -1,13 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         res = []
-        chars = {2:\abc\, 3:\def\, 4:\ghi\, 5:\jkl\, 6:\mno\, 7:\pqrs\, 8:\tuv\, 9:\wxyz\}
-        def dfs(i, word):
+        if not digits:
+            return []
+        digitMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        def backtrack(i, curStr):
             if i >= len(digits):
-                res.append(word)
+                res.append(curStr)
                 return
-            for c in chars[int(digits[i])]:
-                dfs(i+1, word + c)
-        if digits:
-            dfs(0, \\)
-        return res
+            for d in digitMap[digits[i]]:
+                backtrack(i+1, curStr + d)
+        backtrack(0, "")
+        return res 
+
+                
