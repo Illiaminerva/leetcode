d5f22cf9ef2770fb4455bd691c7e194f5c0d6c86
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-11-13 15:56:03 -0800

    [133] [Time Beats: 81.12%] [Memory Beats: 24.39%] - LeetPush

--- 133-Clone-Graph.py
+++ 133-Clone-Graph.py
@@ -1,23 +1,34 @@
-\\\
+"""
 # Definition for a Node.
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-\\\
+"""
 
 from typing import Optional
+from collections import defaultdict 
 class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        oldtonew = {}
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        queue = [node]
+        old_to_new = {node: Node(node.val)}
+        
+        while queue:
+            cur_node = queue.pop(0)
+            
+            for neighbor in cur_node.neighbors:
+                if neighbor not in old_to_new:  # Create new node only if it hasn't been created before
+                    old_to_new[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                # Connect the current node to its neighbor
+                old_to_new[cur_node].neighbors.append(old_to_new[neighbor])
+                
+        return old_to_new[node]
 
-        def dfs(node):
-            if node in oldtonew:
-                return oldtonew[node]
-            copy = Node(node.val)
-            oldtonew[node] = copy
-            for neighbor in node.neighbors:
-                copy.neighbors.append(dfs(neighbor))
-            return copy
-        return dfs(node) if node else None
+            
+            
+        
+        
         
