02a420e817cb65b9dfc37515b43e0d2a28dfaaa5
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-11-18 14:24:20 -0800

    [210] [Time Beats: 42.12%] [Memory Beats: 19.60%] - LeetPush

--- 210-Course-Schedule-II.py
+++ 210-Course-Schedule-II.py
@@ -1,27 +1,38 @@
+from collections import defaultdict
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        from collections import defaultdict
-        prereq = defaultdict(list)
-        for crs, pre in prerequisites:
-            prereq[crs].append(pre)
-        cycle, visit = set(), set()
+        visited = set()
+        cycle = set()
+        adjList = defaultdict(list)
         output = []
+        for crs, pre in prerequisites:
+            adjList[crs].append(pre)
+        
         def dfs(crs):
             if crs in cycle:
                 return False
-            if crs in visit: 
+            if crs in visited:
                 return True
             cycle.add(crs)
-            for pre in prereq[crs]:
+            for pre in adjList[crs]:
                 if not dfs(pre):
                     return False
             cycle.remove(crs)
-            visit.add(crs)
+            visited.add(crs)
             output.append(crs)
             return True
+
+
         for crs in range(numCourses):
             if not dfs(crs):
                 return []
         return output
 
+
+
         
+
+            
+            
+
+
