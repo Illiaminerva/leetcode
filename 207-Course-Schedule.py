a27954688ee1bc70fcba97ce559218b9ccecb7c9
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-11-18 14:12:07 -0800

    [207] [Time Beats: 11.61%] [Memory Beats: 42.78%] - LeetPush

--- 207-Course-Schedule.py
+++ 207-Course-Schedule.py
@@ -1,24 +1,22 @@
 from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        dep_list = defaultdict(list)
-        for crs, pre in prerequisites:
-            dep_list[crs].append(pre)
+        adjList = defaultdict(list)
         visited = set()
         def dfs(crs):
             if crs in visited:
                 return False
             visited.add(crs)
-            for pre in dep_list[crs]:
-                if not dfs(pre):
+            for dep in adjList[crs]:
+                if not dfs(dep):
                     return False
             visited.remove(crs)
-            dep_list[crs] = []
+            adjList[crs] = []
             return True
+        for crs, dep in prerequisites:
+            adjList[crs].append(dep)
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
         return True
-
-
-
+                
