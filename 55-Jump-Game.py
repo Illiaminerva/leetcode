6ba3b6854bec0684b637e9ef108a99e05c65dab2
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-11-21 15:02:35 -0800

    [55] [Time Beats: 31.38%] [Memory Beats: 62.08%] - LeetPush

--- 55-Jump-Game.py
+++ 55-Jump-Game.py
@@ -1,7 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums) - 1
-        for i in range(len(nums)-2, -1, -1):
-            if i + nums[i] >= goal:
-                goal = i
-        return goal == 0
+        max_reach = 0
+        for i in range(len(nums)):
+            if max_reach < i:
+                return False
+            max_reach = max(max_reach, i + nums[i])
+        return True
