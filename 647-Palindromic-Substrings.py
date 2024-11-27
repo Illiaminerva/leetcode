9f515f4d1cd1c286b4370f9501d554d82967aa0c
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-11-27 14:40:34 -0800

    [647] [Time Beats: 42.63%] [Memory Beats: 24.38%] - LeetPush

--- 647-Palindromic-Substrings.py
+++ 647-Palindromic-Substrings.py
@@ -1,14 +1,16 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        num_pal = 0
+        total = 0
         for i in range(len(s)):
             l, r = i, i
-            while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
-                num_pal += 1
-                l, r = l-1, r+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                total += 1
+                l -= 1
+                r += 1
         for i in range(len(s)-1):
             l, r = i, i+1
-            while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
-                num_pal += 1
-                l, r = l-1, r+1      
-        return num_pal     
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                total += 1
+                l -= 1
+                r += 1
+        return total
