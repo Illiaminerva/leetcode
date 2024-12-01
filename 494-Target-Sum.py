ec356891e01026bd9a80727ce82f5c1ce6fbcebe
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-12-01 13:45:28 -0800

    [494] [Time Beats: 12.36%] [Memory Beats: 13.48%] - LeetPush

--- 494-Target-Sum.py
+++ 494-Target-Sum.py
@@ -1,14 +1,16 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        cache = {}
-        def dfs(i, curSum):
+        dp = {}
+        def dfs(i, r):
+            if r == target and i == len(nums):
+                return 1
             if i == len(nums):
-                if curSum == target:
-                    return 1
-                else:
-                    return 0
-            if (i, curSum) in cache:
-                return cache[(i, curSum)]
-            cache[(i, curSum)] = dfs(i+1, curSum + nums[i]) + dfs(i+1, curSum - nums[i])
-            return cache[(i, curSum)]
+                return 0
+            if (i, r) in dp:
+                return dp[(i, r)]
+            dp[(i, r)] = dfs(i+1, r - nums[i]) + dfs(i+1, r + nums[i])
+            return dp[(i, r)]
         return dfs(0, 0)
+
+
+        
