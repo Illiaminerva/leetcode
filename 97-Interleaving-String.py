eac8360a2fb09ffa060d059d2de523ae4ae02eeb
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-12-01 14:56:50 -0800

    [97] [Time Beats: 62.80%] [Memory Beats: 14.09%] - LeetPush

--- 97-Interleaving-String.py
+++ 97-Interleaving-String.py
@@ -1,15 +1,18 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) != len(s3):
+        if len(s3) != len(s1) + len(s2):
             return False
-        dp = [[False] * (len(s2) + 1) for i in range(len(s1) + 1)]
-        dp[len(s1)][len(s2)] = True
-        for i in range(len(s1), -1, -1):
-            for j in range(len(s2), -1, -1):
-                print(i,j)
-                if i < len(s1) and dp[i+1][j] and s1[i] == s3[i+j]:
-                    dp[i][j] = True
-                if j < len(s2) and dp[i][j+1] and s2[j] == s3[i+j]:
-                    dp[i][j] = True  
-        print(dp)
-        return dp[0][0]              
+        dp = {}
+        def dfs(i, j, k):
+            if i == len(s1) and j == len(s2):
+                return True
+            if i == len(s1):
+                return s2[j:] == s3[k:]
+            if j == len(s2):
+                return s1[i:] == s3[k:]
+            if (i, j) in dp:
+                return dp[(i, j)]
+            result = (s1[i] == s3[k] and dfs(i+1, j, k+1)) or (s2[j] == s3[k] and dfs(i, j+1, k+1))
+            dp[(i, j)] = result
+            return result
+        return dfs(0, 0, 0)
