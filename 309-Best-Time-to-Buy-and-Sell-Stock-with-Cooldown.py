c6a27993206f7efd2e7c2bc1334a4a489c63f126
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-12-01 13:01:40 -0800

    [309] [Time Beats: 43.92%] [Memory Beats: 17.86%] - LeetPush

--- 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices):
         dp = {}
         def dfs(i, buying):
             if i >= len(prices):
@@ -8,10 +8,10 @@ class Solution:
                 return dp[(i, buying)]
             cooldown = dfs(i+1, buying)
             if buying:
-                buy = dfs(i+1, False) - prices[i]
-                dp[(i, buying)] = max(buy, cooldown)
+                buy = dfs(i+1, not buying) - prices[i]
+                dp[(i, buying)] = max(cooldown, buy)
             else:
-                sell = dfs(i+2, True) + prices[i]
-                dp[(i, buying)] = max(sell, cooldown)
-            return dp[(i,buying)]
+                sell = dfs(i+2, not buying) + prices[i]
+                dp[(i, buying)] = max(cooldown, sell)
+            return dp[(i, buying)]
         return dfs(0, True)
