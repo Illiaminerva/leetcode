a4526b2fbe2e45e90e862033d471facd8905c008
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-12-02 11:10:56 -0800

    [39] [Time Beats: 67.38%] [Memory Beats: 5.91%] - LeetPush

--- 39-Combination-Sum.py
+++ 39-Combination-Sum.py
@@ -1,20 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
         result = []
         subset = []
-        def backtrack(i, cur_sum):
-            if i >= len(candidates):
-                return
-            if cur_sum == target:
+        def dfs(i, curSum):
+            if curSum == target:
                 result.append(subset.copy())
                 return
-            if cur_sum > target:
+            if i == len(candidates):
                 return
-            subset.append(candidates[i])
-            backtrack(i, cur_sum + candidates[i])
-            subset.pop()
-            backtrack(i + 1, cur_sum)
-        backtrack(0, 0)
+            if curSum + candidates[i] <= target:
+                subset.append(candidates[i])
+                dfs(i, curSum + candidates[i])
+                subset.pop()
+            dfs(i+1, curSum)
+        dfs(0, 0)
         return result
 
                 
