f9df7b4b90c6638cf64035f4c7aa09c662227d2f
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-12-24 11:07:38 -0800

    [787] [Time Beats: 70.24%] [Memory Beats: 17.46%] - LeetPush

--- 787-Cheapest-Flights-Within-K-Stops.py
+++ 787-Cheapest-Flights-Within-K-Stops.py
@@ -2,20 +2,21 @@ from collections import defaultdict, deque
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         flightDict = defaultdict(list)
-        for f, t, price in flights:
-            flightDict[f].append((t, price))
-        visited = [float('inf')] * n
-        visited[src] = 0
+        for f, t, p in flights:
+            flightDict[f].append((t, p))
+        visit_cost = [float("+inf")] * n
+        visit_cost[src] = 0
         queue = deque([(src, 0)])
         for i in range(k+1):
             curLen = len(queue)
-            for j in range(curLen):
-                city, curPrice = queue.popleft()
-                for dest, price in flightDict[city]:
-                    if curPrice + price < visited[dest]:
-                        visited[dest] = curPrice + price
-                        queue.append((dest, curPrice + price))
-        return visited[dst] if visited[dst] < float("+inf") else -1
+            for _ in range(curLen):
+                node, price = queue.popleft()
+                for flightDest, extra in flightDict[node]:
+                    if price + extra < visit_cost[flightDest]:
+                        visit_cost[flightDest] = price + extra
+                        queue.append((flightDest, price + extra))
+            print(queue)
+        return visit_cost[dst] if visit_cost[dst] < float("+inf") else -1
 
 
 
