42ff13f3b620385a5f4e208a030f2346a411e35e
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2024-12-29 19:48:57 -0800

    [79] [Time Beats: 24.51%] [Memory Beats: 14.55%] - LeetPush

--- 79-Word-Search.py
+++ 79-Word-Search.py
@@ -1,41 +1,17 @@
-# 1. Find a match when the first letter is the same 
-# 2. Search the four dimensions within the boundaries 
-    # -> if match: continue 
-    # -> if not match: go to different direction
-
-    # -> go through (x,y)+[(1,0), (0,1), (-1,0), (0,-1)]
-    # dfs(x, y, i): 
-        # if i == len(words): 
-            # return True 
-        # check if visited? 
-            # if yes -> return 
-        # check if it is a match for word[i]: 
-            # if yes: 
-            # put (x,y) into visited 
-            # if not
-                #add it into visited: {S}
-            # recurse(x, y, i+1) within boundaries 
-            # free up the specific visited value 
-        #return False 
-
-
-# visited: capture position that we found 
-
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-        visited = set() #(r,c)
-        def dfs(r, c, i):
-            if i == len(word):
+        index_set = set()
+        def dfs(i, j, k):
+            if k == len(word):
                 return True
-            if not 0 <= r < rows or not 0 <= c < cols or (r, c) in visited or board[r][c] != word[i]:
+            if not 0 <= i < len(board) or not 0 <= j < len(board[0]) or (i, j) in index_set or board[i][j] != word[k]:
                 return False
-            visited.add((r,c))
-            result = dfs(r+1, c, i+1) or dfs(r-1, c, i+1) or dfs(r, c+1, i+1) or dfs(r, c-1, i+1)
-            visited.remove((r,c))
+            index_set.add((i,j))
+            result = dfs(i+1, j, k+1) or dfs(i-1, j, k+1) or dfs(i, j+1, k+1) or dfs(i, j-1, k+1)
+            index_set.remove((i,j))
             return result
-        for r in range(rows):
-            for c in range(cols):
-                if dfs(r,c,0):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(i, j, 0):
                     return True
         return False
