ab4c0c498585478edbe147f6192492384d9fc6e3
Illia Lazurenko <83811142+Illiaminerva@users.noreply.github.com>
2025-01-06 18:19:37 -0800

    [2483] [Time Beats: 93.39%] [Memory Beats: 53.82%] - LeetPush

--- 2483-Minimum-Penalty-for-a-Shop.py
+++ 2483-Minimum-Penalty-for-a-Shop.py
@@ -1,14 +1,14 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        min_penalty = 0
-        cur_penalty = 0
-        min_index = len(customers)
-        for i in range(len(customers)-1, -1, -1):
-            if customers[i] == "Y":
-                cur_penalty += 1
+        curProfit = 0
+        maxClos = 0
+        maxProfit = 0
+        for i, customer in enumerate(customers):
+            if customer == "Y":
+                curProfit += 1
+                if curProfit > maxProfit:
+                    maxClos = i + 1
+                    maxProfit = curProfit
             else:
-                cur_penalty -= 1
-            if cur_penalty <= min_penalty:
-                min_index = i
-                min_penalty = cur_penalty
-        return min_index
+                curProfit -= 1
+        return maxClos
